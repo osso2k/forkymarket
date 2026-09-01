f54f86e0157ece9e67b4168e989d2ee3d93da2c3
osso2k <ossoble817@gmail.com>
2026-09-01 21:53:58 +0300

    ssl and dat

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -9,8 +9,8 @@ export const pool = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT)
-
+    port: Number(process.env.DB_PORT),
+    ssl: process.env.DB_SSL == "true" ? {rejectUnauthorized: false} : false
 })
 export const connectDB = async ()=>{
     try {
