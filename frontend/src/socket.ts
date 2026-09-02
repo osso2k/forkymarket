d7b8a163509a0835c6f77646d0c8e92e12e70400
osso2k <ossoble817@gmail.com>
2026-09-02 16:31:43 +0300

    bug fix: socket used token before user logged in leading to infinite loading page

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,7 +1,7 @@
-const token = localStorage.getItem("token") || ""
 
 export const socket = new WebSocket(import.meta.env.VITE_BACKEND_WSS || 'ws://localhost:8080')
 
 socket.onopen = () => {
+    const token = localStorage.getItem("token") || ""
     socket.send(JSON.stringify({ type: "auth", token }))
 }
